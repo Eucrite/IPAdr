7b495dc778317e0271d0ec80990078033cec3e1c
Zebin Yang <tom.zebinyang@gmail.com>
2017-06-24 19:38:42 -0400

    Add files via upload

---
+++ ipAPP/IpAdr/IpAdr/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace IpAdr
+{
+    partial class frmIpAdr
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+         this.lblIP = new System.Windows.Forms.Label();
+         this.lblLocation = new System.Windows.Forms.Label();
+         this.dataGridView1 = new System.Windows.Forms.DataGridView();
+         this.colFlag = new System.Windows.Forms.DataGridViewTextBoxColumn();
+         this.colCountryName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+         this.colIpAdr = new System.Windows.Forms.DataGridViewTextBoxColumn();
+         ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+         this.SuspendLayout();
+         // 
+         // lblIP
+         // 
+         this.lblIP.AutoSize = true;
+         this.lblIP.Location = new System.Drawing.Point(84, 41);
+         this.lblIP.Name = "lblIP";
+         this.lblIP.Size = new System.Drawing.Size(48, 13);
+         this.lblIP.TabIndex = 0;
+         this.lblIP.Text = "Your IP: ";
+         // 
+         // lblLocation
+         // 
+         this.lblLocation.AutoSize = true;
+         this.lblLocation.Location = new System.Drawing.Point(56, 54);
+         this.lblLocation.Name = "lblLocation";
+         this.lblLocation.Size = new System.Drawing.Size(76, 13);
+         this.lblLocation.TabIndex = 1;
+         this.lblLocation.Text = "You Location: ";
+         // 
+         // dataGridView1
+         // 
+         this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+         this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.colFlag,
+            this.colCountryName,
+            this.colIpAdr});
+         this.dataGridView1.Location = new System.Drawing.Point(35, 108);
+         this.dataGridView1.Name = "dataGridView1";
+         this.dataGridView1.Size = new System.Drawing.Size(346, 233);
+         this.dataGridView1.TabIndex = 2;
+         // 
+         // colFlag
+         // 
+         this.colFlag.HeaderText = "Flag";
+         this.colFlag.Name = "colFlag";
+         this.colFlag.ReadOnly = true;
+         // 
+         // colCountryName
+         // 
+         this.colCountryName.HeaderText = "Country Name";
+         this.colCountryName.Name = "colCountryName";
+         this.colCountryName.ReadOnly = true;
+         // 
+         // colIpAdr
+         // 
+         this.colIpAdr.HeaderText = "IP Address";
+         this.colIpAdr.Name = "colIpAdr";
+         this.colIpAdr.ReadOnly = true;
+         // 
+         // frmIpAdr
+         // 
+         this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+         this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+         this.ClientSize = new System.Drawing.Size(464, 353);
+         this.Controls.Add(this.dataGridView1);
+         this.Controls.Add(this.lblLocation);
+         this.Controls.Add(this.lblIP);
+         this.Name = "frmIpAdr";
+         this.Text = "IpAdr";
+         this.Load += new System.EventHandler(this.frmIpAdr_Load);
+         ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+         this.ResumeLayout(false);
+         this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblIP;
+        private System.Windows.Forms.Label lblLocation;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colFlag;
+      private System.Windows.Forms.DataGridViewTextBoxColumn colCountryName;
+      private System.Windows.Forms.DataGridViewTextBoxColumn colIpAdr;
+   }
+}
+
---
+++ ipAPP/IpAdr/IpAdr/Form1.cs
@@ -0,0 +1,84 @@
+﻿/*******************************************************************/
+/**   PROGRAM: Form1.cs(Change name after)                        **/
+/**                                                               **/
+/**   Program Creation: Tom Yang                                  **/
+/**   DATE: 6/24/2017                                             **/
+/**   PURPOSE:                                                    **/
+/*******************************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Diagnostics;
+
+namespace IpAdr
+{
+   public partial class frmIpAdr : Form
+   {
+      public frmIpAdr()
+      {
+         /**********************************************************/
+         /** IMPORTANT                                            **/
+         /**********************************************************/
+         InitializeComponent();
+      }
+
+      private void frmIpAdr_Load(object sender, EventArgs e)
+      {
+         /** call function to get user ip                         **/
+         string ip = getUserIp();
+         /** append ip to label IP                                **/
+         lblIP.Text += ip;
+
+         string country = getUserCountry(ip);
+         lblLocation.Text += country;
+      }
+
+      /** Function for getting user ip address                    **/
+      private string getUserIp()
+      {
+         /** stores the ip content from the web into a string     **/
+         string myIp = new WebClient().DownloadString("http://checkip.dyndns.org");
+         /** for loop to cut the front                            **/
+         for (int i=0; i < myIp.Length; i++)
+         {
+            if (myIp[i]=='A' && myIp.Substring(i, 7)=="Address")
+            {
+               myIp = myIp.Substring(i+9, myIp.Length-i-9);
+               /** for loop to cut the back                       **/
+               for (int j=0; j<myIp.Length; j++)
+               {
+                  if(myIp[j] == '<')
+                  {
+                     myIp = myIp.Substring(0, j);
+                     break;
+                  }
+               }
+               break;
+            }
+         }
+         return myIp;
+      }
+
+      private string getUserCountry(string ip)
+      {
+         string country = new WebClient().DownloadString("http://www.ip2nation.com/");
+         int size = country.IndexOf("IP: " + ip);
+         country = country.Substring(size, country.Length-size);
+         size = country.IndexOf('>');
+         country = country.Substring(size, country.Length - size);
+
+
+         size = country.IndexOf('<');
+         country = country.Substring(1, size-1);
+         return country;
+      }
+   }
+}
